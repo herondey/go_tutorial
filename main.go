a0ae2cc248b071f06ce05039ca73faa24a182de1
TakamasaNishizawa <nishizawa@yoshidumi.co.jp>
2018-08-13 18:21:54 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/herondey/go_tutorial/basic1"
+	_ "github.com/herondey/go_tutorial/basic2"
+	"github.com/herondey/go_tutorial/basic3"
+)
+
+func main() {
+	basic3.Makingslices()
+	basic3.Nilslices()
+	basic3.Slicelencap()
+	basic3.Slicebounds()
+	basic3.Sliceliterals()
+	basic3.Slicespointers()
+	basic3.Slicesgo()
+	basic3.Arraygo()
+	basic3.Structliterals()
+	basic3.Structpointers()
+	basic3.Structfields()
+	basic3.Structs()
+	basic3.Pointers()
+	/*
+		basic2.Defermulti()
+		basic2.Defergo()
+		basic2.Switchwithnocondition()
+		basic2.Switchevaluationorder()
+		basic2.Switchgo()
+		basic2.Exerciseloopsandfunctions()
+		basic2.Ifandelse()
+		basic2.Ifwithashortstatement()
+		basic2.Ifgo()
+		basic2.Forisgoswhile()
+		basic2.Forcontinued()
+		basic2.Forgo()
+	*/
+	/*
+		basic1.Numericconstants()
+		basic1.Constants()
+		basic1.Typeinference()
+		basic1.Typecomversions()
+		basic1.Zero()
+		basic1.Basictypes()
+		basic1.Shortvariabledeclarations()
+		basic1.Variableswithinitializers()
+		basic1.Variables()
+		basic1.Namedresult()
+		basic1.Multipleresults()
+		basic1.Functionscontinued()
+		basic1.Functions()
+		basic1.Exportednames()
+		basic1.Imports()
+		basic1.Sandbox()
+		basic1.Packages()
+	*/
+	fmt.Println("hello 世界")
+}
